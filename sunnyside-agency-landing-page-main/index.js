dcbf6570293be2df06746c786393e23a9e58de1a
Grey <71736626+E-code804@users.noreply.github.com>
2022-07-28 20:43:12 -0400

    Delete sunnyside-agency-landing-page-main directory

--- sunnyside-agency-landing-page-main/index.js
+++
@@ -1,9 +0,0 @@
-// Navbar JS
-
-const hamburger = document.querySelector(".hamburger");
-const linksBtns = document.querySelector(".links-btns");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  linksBtns.classList.toggle("active");
-});
